de1399775ac05b8130dc606fc75fd0a1b512ddf0
David Hilscher <davidhilscher@gmail.com>
2017-11-16 09:08:30 -0800

    Done.

---
+++ __tests__/string-compression.spec.js
@@ -0,0 +1,10 @@
+const stringCompression = require("../lib/string-compression.js");
+
+describe("stringCompression", () => {
+  describe("character count", () => {
+    it("should return a3b3c3", () => {
+      const compress = stringCompression("aaabbbccc");
+      expect(compress).toEqual("a3b3c3");
+    });
+  });
+});
